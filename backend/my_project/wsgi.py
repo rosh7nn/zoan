90026518b4112850b47b90eb9a79fa30c1c71c19
Roshan <124807359+rosh7nn@users.noreply.github.com>
2025-06-10 03:29:33 +0530

    wsgi.py

--- backend/my_project/wsgi.py
+++ backend/my_project/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
 
 application = get_wsgi_application()
+
+# Run superuser creation script on startup
+try:
+    from . import create_superuser
+except Exception as e:
+    print("Superuser creation script error:", e)
+
